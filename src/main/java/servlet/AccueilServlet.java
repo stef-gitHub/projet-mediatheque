102fa55e8ffcba30dfcac1c10999d913eb4165b7
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-09-28 21:17:04 +0200

    ADD popper + jquery

--- src/main/java/servlet/AccueilServlet.java
+++ src/main/java/servlet/AccueilServlet.java
@@ -10,7 +10,6 @@ public class AccueilServlet extends HttpServlet {
 
         public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
             this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/accueil.jsp" ).forward( request, response );
         }
 }
