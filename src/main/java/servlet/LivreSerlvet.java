52c860106c7058656885a52591668381e389a2cb
MarjorieMeunier <61453391+MarjorieMeunier@users.noreply.github.com>
2020-10-05 09:27:44 +0100

    - Deleted DAO

--- src/main/java/servlet/LivreSerlvet.java
+++ src/main/java/servlet/LivreSerlvet.java
@@ -1,13 +1,10 @@
 package servlet;
 
-import dao.LivreDAO;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class LivreSerlvet extends HttpServlet {
 
